3fb54f55e14dabb6eec93419fd844b6fdfb0bba4
Nigel.Zheng <nigel.zheng@emerson.com>
2023-10-12 11:11:30 +0800

    apply test-sets gradle plugin

--- src/integration-test/java/pl/dziedziul/contractsincustomsourceset/ApplicationIntegrationTest.java
+++ src/integration-test/java/pl/dziedziul/contractsincustomsourceset/ApplicationIntegrationTest.java
@@ -1,13 +1,15 @@
 package pl.dziedziul.contractsincustomsourceset;
 
+import groovy.util.logging.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class ApplicationIntegrationTest {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
