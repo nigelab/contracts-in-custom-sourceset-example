35bc61d56d4255244db04c89295a8538404e5150
Nigel.Zheng <nigel.zheng@emerson.com>
2023-10-12 09:29:42 +0800

    re-organize test

---
+++ src/test/java/pl/dziedziul/contractsincustomsourceset/ApplicationTest.java
@@ -0,0 +1,14 @@
+package pl.dziedziul.contractsincustomsourceset;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by Nigel on 10/12/2023.
+ *
+ * @author Nigel
+ */
+class ApplicationTest {
+  @Test
+  void unitTest() {
+  }
+}
