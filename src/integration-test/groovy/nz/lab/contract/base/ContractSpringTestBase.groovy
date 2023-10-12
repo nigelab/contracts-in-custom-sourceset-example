83d638bf134144099390a25d79cc407ad424fd83
Nigel.Zheng <nigel.zheng@emerson.com>
2023-10-12 13:41:21 +0800

    correct contract test base

--- src/integration-test/groovy/nz/lab/contract/base/ContractSpringTestBase.groovy
+++ src/integration-test/groovy/nz/lab/contract/base/ContractSpringTestBase.groovy
@@ -6,14 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier
 import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder
+import pl.dziedziul.contractsincustomsourceset.Application
 import pl.dziedziul.contractsincustomsourceset.CarResource
 import spock.lang.Specification
 
 @Slf4j
 @DirtiesContext
 @AutoConfigureMessageVerifier
+@ContextConfiguration(classes = Application)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 class ContractSpringTestBase extends Specification {
     @Autowired
--- src/integration-test/groovy/nz/lab/contract/base/ContractTestBase.groovy
+++ src/integration-test/groovy/nz/lab/contract/base/ContractTestBase.groovy
@@ -4,11 +4,14 @@ import io.restassured.module.mockmvc.RestAssuredMockMvc
 import lombok.extern.slf4j.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import pl.dziedziul.contractsincustomsourceset.Application
 import spock.lang.Specification
 
 @WebMvcTest
 @Slf4j
+@ContextConfiguration(classes = Application)
 abstract class ContractTestBase extends Specification {
     @Autowired
     private MockMvc mockMvc
